9cc09cd8e437fb8952f756012aab6dfe312896ac
Jo Fischer <josephinefischer9@gmail.com>
2020-05-21 13:52:18 -0400

    Add initialize

--- lib/google_scrape.rb
+++ lib/google_scrape.rb
@@ -1,2 +1,9 @@
 class GoogleScrape
+  attr_accessor :page
+
+  def initialize
+    @page = Nokogiri::HTML(HTTParty.get(
+      "https://www.google.com/search?q=#{coffee.slug}"
+    ).body)
+  end
 end
